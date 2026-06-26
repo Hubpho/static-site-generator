e238af1d54889ebc1ffb5930f8fc96ef3ccf479e
Gitpho <gitpho@gmail.com>
2026-06-26 16:13:57 -0400

    2.6 Nodes | TextNode to HTMLNode

--- src/htmlnode.py
+++ src/htmlnode.py
@@ -45,14 +45,19 @@ class LeafNode(HTMLNode):
 
 class ParentNode(HTMLNode):
     def __init__(
-        self, tag: str, children: list["HTMLNode"], props: dict[str, str] | None = None
-    ):
+        self, tag: str, children: list[HTMLNode], props: dict[str, str] | None = None
+    ) -> None:
         super().__init__(tag, None, children, props)
 
-    def to_html(self):
-        if not self.tag:
-            raise ValueError("not self.tag")
-        if not self.children:
-            raise ValueError("not children")
+    def to_html(self) -> str:
+        if self.tag is None:
+            raise ValueError("invalid HTML: no tag")
+        if self.children is None:
+            raise ValueError("invalid HTML: no children")
+        children_html = ""
         for child in self.children:
-            return f"<{self.tag}>{child.to_html()}</{self.tag}>"
+            children_html += child.to_html()
+        return f"<{self.tag}{self.props_to_html()}>{children_html}</{self.tag}>"
+
+    def __repr__(self) -> str:
+        return f"ParentNode({self.tag}, children: {self.children}, {self.props})"
